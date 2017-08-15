5303ab6f5e67b12421ba8b3439cf76200972896e
DreamerJane <zjj14zjj@gmail.com>
2017-08-16 00:52:29 +0800

    change the class name of the Revese & format the code

--- src/java/easy/TwoSum.java
+++ src/java/easy/TwoSum.java
@@ -9,17 +9,17 @@ public class TwoSum {
 	 * Because nums[0] + nums[1] = 2 + 7 = 9,
 	 * return [0, 1].
 	*/
-    public int[] twoSum(int[] nums, int target) {
-        int result[] = new int[2];
-        for(int i = 0; i < nums.length - 1; i++){
-            for(int j = i + 1; j < nums.length; j++){
-                if(target == nums[i] + nums[j]){
-                    result[0] = i;
-                    result[1] = j;
-                    break;
-                }
-            }
-        }
-        return result;
-    }
+	public int[] twoSum(int[] nums, int target) {
+		int result[] = new int[2];
+		for (int i = 0; i < nums.length - 1; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				if (target == nums[i] + nums[j]) {
+					result[0] = i;
+					result[1] = j;
+					break;
+				}
+			}
+		}
+		return result;
+	}
 }
